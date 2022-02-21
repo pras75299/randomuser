96cbbfbd3176fc19733f53a50e5c0897b9f015ca
pras75299 <singh.prashantking@gmail.com>
2022-02-22 01:24:35 +0530

    structure of layout and useState default values use

--- src/App.js
+++ src/App.js
@@ -24,7 +24,56 @@ function App() {
       <div className="block bcg-black"></div>
       <div className="block">
         <div className="container">
-          <img src={defaultImage} alt="random-user" className="user-img" />
+          <img
+            src={(person && person.image) || defaultImage}
+            alt="random-user"
+            className="user-img"
+          />
+          <p className="user-title">My {title} is</p>
+          <p className="user-value">{value}</p>
+          <div className="values-list">
+            <button
+              className="icon"
+              data-label="name"
+              onMouseOver={handleValue}
+            >
+              <FaUser />
+            </button>
+            <button
+              className="icon"
+              data-label="email"
+              onMouseOver={handleValue}
+            >
+              <FaEnvelopeOpen />
+            </button>
+            <button className="icon" data-label="age" onMouseOver={handleValue}>
+              <FaCalendarTimes />
+            </button>
+            <button
+              className="icon"
+              data-label="street"
+              onMouseOver={handleValue}
+            >
+              <FaMap />
+            </button>
+            <button
+              className="icon"
+              data-label="phone"
+              onMouseOver={handleValue}
+            >
+              <FaPhone />
+            </button>
+            <button
+              className="icon"
+              data-label="password"
+              onMouseOver={handleValue}
+            >
+              <FaLock />
+            </button>
+          </div>
+          <button className="btn" type="button">
+            {loading} ? 'loading' : 'random user';
+          </button>
         </div>
       </div>
     </main>
